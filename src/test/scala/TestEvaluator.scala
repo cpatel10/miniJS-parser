0946514b24e27580188ffafb8091ad05c522274d
emishparii/em <emishparii@icstars.org/em>
2016-04-09 19:59:02 -0500

    created sample evaluator tests

--- src/test/scala/TestEvaluator.scala
+++ src/test/scala/TestEvaluator.scala
@@ -1,8 +1,24 @@
+import edu.luc.cs.laufer.cs473.expressions._
+import TestFixtures._
 import org.scalatest.FunSuite
+import scala.util.Success
 
 /**
   * Created by Chingari on 4/8/2016.
   */
 class TestEvaluator extends FunSuite {
+
+  test("evaluate twoAssignments") {
+    val result = Evaluator.evaluate(simpleStatementList)
+    assert(result === Success(Num(0)))
+    assert(Evaluator.store.get("x").get === Cell(Num(5)))
+    assert(Evaluator.store.get("y").get === Cell(Num(7)))
+  }
+
+  test("evaluate singleVariable") {
+    val result = Evaluator.evaluate(simpleVariable)
+    assert(result === Success(Num(5)))
+    assert(Evaluator.store.get("x").get === Cell(Num(5)))
+  }
 
 }
